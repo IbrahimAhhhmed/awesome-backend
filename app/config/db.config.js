e70c9a3fc9ae0edc48aef07e1e9d8a345ee61f83
IbrahimAhhhmed <ibrahimahmed6941@gmail.com>
2021-07-07 12:55:36 +0500

    dbConfig updated.

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/product"
+    // url: "mongodb://localhost:27017/product"
+    url: "mongodb+srv://ibrahimahmed:eidmubarak@cluster0.6tr0f.mongodb.net/test"
 };
