2fc55d81e8d6dfacbc985b15682604bdecefc535
IbrahimAhhhmed <ibrahimahmed6941@gmail.com>
2021-07-07 14:26:50 +0500

    Mongodb srv updated

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     // url: "mongodb://localhost:27017/product"
-    url: "mongodb://ibrahimahmed:eidmubarak@cluster0.6tr0f.mongodb.net/test/product"
+    url: "mongodb+srv://ibrahimahmed:eidmubarak@cluster0.6tr0f.mongodb.net/product?retryWrites=true&w=majority"
 };
